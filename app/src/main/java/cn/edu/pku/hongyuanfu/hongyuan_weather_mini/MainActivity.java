b96f1d716f19f51bb09bf696e1b051425ff896db
hongyuanfu <1771593876@qq.com>
2018-10-02 15:06:58 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/hongyuanfu/hongyuan_weather_mini/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.pku.hongyuanfu.hongyuan_weather_mini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
