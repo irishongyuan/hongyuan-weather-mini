0439a430dc0828e8f4f4f67dd42fb40f5fa77afe
hongyuanfu <1771593876@qq.com>
2018-10-23 16:13:52 +0800

    simply add city list to show and choose

--- app/src/main/java/cn/edu/pku/hongyuanfu/hongyuan_weather_mini/SelectCity.java
+++ app/src/main/java/cn/edu/pku/hongyuanfu/hongyuan_weather_mini/SelectCity.java
@@ -3,10 +3,17 @@ package cn.edu.pku.hongyuanfu.hongyuan_weather_mini;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cn.edu.pku.hongyuanfu.app.MyApplication;
 import cn.edu.pku.hongyuanfu.bean.City;
@@ -14,6 +21,12 @@ import cn.edu.pku.hongyuanfu.bean.City;
 public class SelectCity extends Activity implements View.OnClickListener{
 
     private ImageView mBackBtn;
+    private ListView mListView;
+    private List<City> mCityList;
+    private List<String> mCityName = new ArrayList();
+    private List<String> mCityNum = new ArrayList();;
+    private TextView mTitlename;
+    private String mCurCityNum;
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
@@ -22,21 +35,41 @@ public class SelectCity extends Activity implements View.OnClickListener{
         setContentView(R.layout.select_city);
 
         initViews();
+
+        mTitlename = (TextView) findViewById(R.id.title_name);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("MySelectCity", "start");
+                mCurCityNum = mCityNum.get(position);
+                mTitlename.setText("当前城市："+mCityName.get(position));
+                Toast toast = Toast.makeText(SelectCity.this, "你单击了:" + mCityName.get(position),
+                        Toast.LENGTH_SHORT);
+            }
+        });
     }
 
     private void initViews() {
         mBackBtn = (ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
 
-        /*mEditText = (EditText) findViewById(R.id.search_city);
+        /*mEditText = (EditText) findViewById(R.id.search_city);*/
 
-        mList = (ListView) findViewById(R.id.title_list);
+        mListView = (ListView) findViewById(R.id.title_list);
         MyApplication myApplication = (MyApplication) getApplication();
-        cityList = myApplication.getCityList();
-        for (City city : cityList) {
-            filterDateList.add(city);
+        mCityList = myApplication.getCityList();
+
+        for (City city : mCityList) {
+            mCityName.add(city.getCity());
+            mCityNum.add(city.getNumber());
         }
-        myadapter = new Myadapter()*/
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                SelectCity.this, android.R.layout.simple_list_item_1, mCityName);
+        mListView.setAdapter(adapter);
+
+        Log.d("MySelectCity", "setAdapter");
+
     }
 
     @Override
@@ -44,7 +77,8 @@ public class SelectCity extends Activity implements View.OnClickListener{
         switch (v.getId()) {
             case R.id.title_back:
                 Intent i = new Intent();
-                i.putExtra("cityCode", "101160101");
+                Log.d("MySelectCity", mCurCityNum);
+                i.putExtra("cityCode",mCurCityNum);
                 setResult(RESULT_OK, i);
                 finish();
                 break;
